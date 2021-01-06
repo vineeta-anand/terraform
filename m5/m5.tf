1719e4f0e78fa58eacd7a8cef1a0a9aea9d870cf
Vineeta-shastri <vineetashastri193@gmail.com>
2021-01-05 23:49:26 -0500

    removed some folders

--- m5/m5.tf
+++
@@ -1,9 +0,0 @@
-module "ec2-Instance-creating"{
-    source = "/home/ubuntu/terraform/module_folder"
-    ami = "ami-0a91cd140a1fc148a"
-    instance_type = "t2.micro"
-}
-
-output "publicId_created" {
-    value = module.ec2-Instance-creating.public-ip  
-}
